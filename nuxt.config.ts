be8344b4753a9d338c6ba01f9f985dd437c31f1a
Klein Petr <petr@kleinpetr.com>
2023-04-03 12:24:11 +0200

    fix dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,10 @@ export default defineNuxtConfig({
     '@nuxtjs/color-mode',
     '@nuxt/devtools',
   ],
+  sourcemap: {
+    server: true,
+    client: false,
+  },
   experimental: {
     // when using generate, payload js assets included in sw precache manifest
     // but missing on offline, disabling extraction it until fixed
